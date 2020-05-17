5d3e5653b8e83f9a85512f26e103fbed0c8905f1
michaeldavidpryor <mdpcaps@gmail.com>
2020-05-16 22:48:48 -0400

    passed tests

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,13 @@
-// TODO: Write code to define and export the Employee class
+
+
+
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email =email;
+    }
+    getName = () => this.name; getId = () => this.id; getEmail = () => this.email; getRole = () => Employee.name;
+
+}
+module.exports = Employee
